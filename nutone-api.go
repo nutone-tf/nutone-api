472d5e0486bc8b24bb930bdcfb150d3db8f079b7
okvdai <108829133+okvdai@users.noreply.github.com>
2024-04-18 21:51:45 +0200

    Add Access-Control-Allow-Headers header

--- nutone-api.go
+++ nutone-api.go
@@ -335,6 +335,7 @@ func logRequest(r *http.Request) {
 
 func sendJSONResponse(w http.ResponseWriter, resp map[string]interface{}) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "GET, POST")
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Accept", "*/*")
 	jsonResp, _ := json.Marshal(resp)

108f4d203e7e3e9f006584d21c5fb4893f8e2658
okvdai <108829133+okvdai@users.noreply.github.com>
2025-08-09 23:37:19 +0200

    fix: unable to see pulse blade kills

--- src/queries.zig
+++ src/queries.zig
@@ -7,7 +7,7 @@ pub const Create = struct {
         pub const Players = "create table if not exists players (uid text, name text, timestamp timestamp default current_timestamp, primary key (uid, name))";
         pub const Servers = "create table if not exists servers (server_id text primary key, server_name text, owner text, timestamp timestamp default current_timestamp)";
         pub const Matches = "create table if not exists matches (match_id text primary key, server_id text, game_mode text, map text, timestamp timestamp default current_timestamp)";
-        pub const KillData = "create table if not exists kill_data (timestamp timestamp default current_timestamp, match_id text, server_id text, game_time real, attacker_uid text, attacker_weapon text, attacker_titan text, attacker_x real, attacker_y real, attacker_z real, victim_uid text, victim_weapon text, victim_titan text, victim_x real, victim_y real, victim_z real, cause_of_death text, distance real)";
+        pub const KillData = "create table if not exists kill_data (timestamp timestamp default current_timestamp, match_id text, server_id text, game_time real, attacker_uid text, cause_of_death text, attacker_titan text, attacker_x real, attacker_y real, attacker_z real, victim_uid text, victim_weapon text, victim_titan text, victim_x real, victim_y real, victim_z real, cause_of_death text, distance real)";
     };
     pub const Index = struct {
         pub const KillDataTimestamp = "create index if not exists kill_data_timestamp_idx on kill_data(timestamp)";
@@ -21,7 +21,7 @@ pub const Insert = struct {
     pub const Server = "insert or replace into servers (server_id, server_name, owner) values (?1, ?2, (select owner from tokens where token = ?3 limit 1))";
     pub const Player = "insert or replace into players (uid, name) values (?1, ?2), (?3, ?4)";
     pub const Match = "insert or ignore into matches (match_id, server_id, game_mode, map) values (?1, ?2, ?3, ?4)";
-    pub const Kill = "insert into kill_data (match_id, server_id, game_time, attacker_uid, attacker_weapon, attacker_titan, attacker_x, attacker_y, attacker_z, victim_uid, victim_weapon, victim_x, victim_y, victim_z, cause_of_death, distance) values (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16)";
+    pub const Kill = "insert into kill_data (match_id, server_id, game_time, attacker_uid, cause_of_death, attacker_titan, attacker_x, attacker_y, attacker_z, victim_uid, victim_weapon, victim_x, victim_y, victim_z, cause_of_death, distance) values (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16)";
 };
 
 pub const Validate = struct {
@@ -51,16 +51,16 @@ pub const Get = struct {
         pub fn WeaponData(allocator: std.mem.Allocator, filters: Filters) ![:0]u8 {
             if (filters.weapon != null) {
                 const query = std.mem.joinZ(allocator, " ", &.{
-                    "select count(1) as kills, avg(distance) as avg_distance from kill_data where attacker_uid = ?3 and attacker_uid <> victim_uid and attacker_weapon = ?2",
+                    "select count(1) as kills, avg(distance) as avg_distance from kill_data where attacker_uid = ?3 and attacker_uid <> victim_uid and cause_of_death = ?2",
                     if (filters.server != null) "and server_id = ?1" else "",
-                    "group by attacker_weapon",
+                    "group by cause_of_death",
                 });
                 return query;
             } else {
                 const query = std.mem.joinZ(allocator, " ", &.{
-                    "select attacker_weapon, count(1) as kills, avg(distance) as avg_distance from kill_data where attacker_uid = ?3 and attacker_uid <> victim_uid",
+                    "select cause_of_death, count(1) as kills, avg(distance) as avg_distance from kill_data where attacker_uid = ?3 and attacker_uid <> victim_uid",
                     if (filters.server != null) "and server_id = ?1" else "",
-                    "group by attacker_weapon",
+                    "group by cause_of_death",
                 });
                 return query;
             }
@@ -70,9 +70,9 @@ pub const Get = struct {
         pub fn Data(allocator: std.mem.Allocator, filters: Filters) ![:0]u8 {
             if (filters.weapon != null) {
                 const query = try std.mem.joinZ(allocator, " ", &.{
-                    "select players.uid as id, players.name as name, (select count(1) from kill_data where players.uid = kill_data.attacker_uid and kill_data.victim_uid <> kill_data.attacker_uid and kill_data.attacker_weapon = ?2",
+                    "select players.uid as id, players.name as name, (select count(1) from kill_data where players.uid = kill_data.attacker_uid and kill_data.victim_uid <> kill_data.attacker_uid and kill_data.cause_of_death = ?2",
                     if (filters.server != null) "and kill_data.server_id = ?1" else "",
-                    ") as kills, (select avg(kill_data.distance) from kill_data where players.uid = kill_data.attacker_uid and kill_data.attacker_uid <> kill_data.victim_uid and kill_data.attacker_weapon = ?2",
+                    ") as kills, (select avg(kill_data.distance) from kill_data where players.uid = kill_data.attacker_uid and kill_data.attacker_uid <> kill_data.victim_uid and kill_data.cause_of_death = ?2",
                     if (filters.server != null) "and kill_data.server_id = ?1" else "",
                     ") as avg_distance from players where name = (select players.name from players where players.uid = id order by timestamp desc limit 1)",
                     if (filters.server != null) "and id in (select attacker_uid from kill_data where kill_data.server_id = ?1)" else "",
@@ -95,7 +95,7 @@ pub const Get = struct {
         pub fn Count(allocator: std.mem.Allocator, filters: Filters) ![:0]u8 {
             if (filters.weapon != null) {
                 const query = try std.mem.joinZ(allocator, " ", &.{
-                    "select count(1) from (select distinct attacker_uid from kill_data where attacker_weapon = ?2",
+                    "select count(1) from (select distinct attacker_uid from kill_data where cause_of_death = ?2",
                     if (filters.server != null) "and server_id = ?1" else "",
                     ")",
                 });
